4376696d4369f356390a5c641c4001594ea74616
Deepika <deepikaurikiti03@gmail.com>
2023-07-14 23:04:07 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+import openai
+openai.api_key = "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"
+
+app = Flask(__name__)
+app.static_folder = 'static'
+@app.route("/")
+def home():
+    return render_template("index.html")
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    output = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=userText,
+        temperature=0.7,
+        max_tokens=2000,
+        n=1,
+        stop=None,
+    )    
+    return output.choices[0].text
+if __name__ == "__main__":
+    app.run()
+
+
+
